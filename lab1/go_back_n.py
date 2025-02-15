e85a688963fd660e5e9bff3f3d53840eeedbcc77
lyphrowny <strukov.as@edu.spbstu.ru>
2025-02-15 14:06:33 +0300

    Clean up; Add logging; Add EOT packet

--- lab1/go_back_n.py
+++ lab1/go_back_n.py
@@ -1,28 +1,53 @@
-from collections.abc import ByteString, Collection, Container, MutableSequence, Sequence
+import warnings
 import attrs
-import enum
 import random
 import queue
 import time
+from threading import Thread
 
-# STOP_TRANSFER = -1
+import logging
+
+log_level = logging.DEBUG
+
+
+def get_logger(prefix: str):
+    logger = logging.getLogger(prefix)
+
+    # if already created and configured
+    if logger.hasHandlers():
+        return logger
+
+    logger.setLevel(log_level)
+
+    handler = logging.StreamHandler()
+    handler.setLevel(log_level)
+
+    formatter = logging.Formatter(f"{prefix} - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
+
+rlog = get_logger("r")
+slog = get_logger("s")
+plog = get_logger("p")
+
+
 STOP_TRANSFER = "\x00"
-
-
-class Status(enum.IntEnum):
-    ACK = enum.auto()
+# end of transmission
+EOT = -1
 
 
 @attrs.define
 class Packet:
     seq_num: int
     payload: str
-    # status: Status
 
 
 @attrs.define
 class PacketQueue:
-    loss_probability: float
+    loss_probability: float = 0.3
     latency: float = 0.1  # in sec
     _queue: queue.Queue[Packet] = attrs.field(init=False, factory=queue.Queue)
 
@@ -30,174 +55,240 @@ class PacketQueue:
         # emulate network latency
         time.sleep(self.latency)
         if random.random() < self.loss_probability:
-            print(f"{packet} was lost!")
-            return
-        self._queue.put(packet)
+            plog.debug(f"{packet} was lost!")
+        else:
+            self._queue.put(packet)
 
     def recieve(self):
-        # if self._queue.empty():
-        #     return None
         return self._queue.get()
 
     def __bool__(self) -> bool:
-        return not self._queue.empty
+        return not self._queue.empty()
 
 
 @attrs.define
 class Sender:
-    """
+    """Go-Back-N sender
 
     Say we have a message with len 10. Window size is 3.
     Split into batches: [1, 2, 3], [4, 5, 6], [7, 8, 9], [10]
     Then, the seq_nums will be [0, 1, 2], [0, 1, 2], [0, 1, 2], [0]
     So, `seq_num` is the position in a batch. Can be calculated as
-    `message_pos % window_size`, where `message_pos = base_num + window_pos`
+    `message_pos % window_size`
 
     The window is used solely to send `window_size` packets not waiting for ACK
     from reciever. We send `window_size` packets first. For each ACK'ed packet,
-    move the window (`base_num += 1`) but don't update `window_pos`.
+    move the window (`left_bound += 1`) but don't update `m_pos`. `m_pos` points
+    at the not yet sent packet
     left bound     left bound
     v              v
     |0 1 2| 3 -> 0 |1 2 3|
-       ^              ^
-       window pos     window pos
+       ^            ^
+       message pos  message pos
 
-    On each send, update `window_pos`
-    If `window_pos == window_size`, wait for packet from reciever.
-    If timed out, resend the window from positions 0 to `window_pos`.
-        We can time out not only when the `window_pos == window_size`
+    On each iteration check for packet from reciever. Got one? Move the `left_bound`
+    Now we can send another packet (unless we sent all the packetsand waiting for
+    their ACKs). On each send update `m_pos`.
+    If timed out, resend the message from `left_bound`.
     """
 
     sender_to_reciever_ch: PacketQueue
     reciever_to_sender_ch: PacketQueue
     message: str
-    base_num: int = 0  # left boundary of the window
-    window_pos: int = 0  # current position in the window
     window_size: int = 10
-    seq_num: int = 0  # pos in windowed batch
-    sleep_time: float = 0.2
-    timeout_time: float = 1.0
+    timeout: float = 1.0
+    n_sent: int = 0
     num_packets: int = attrs.field(init=False)
-    # _buffer: queue.Queue[str] = attrs.field(init=False, alias="_buffer")
-    _buffer: Sequence[str] = attrs.field(init=False)
 
     def __attrs_post_init__(self):
-        self.message += STOP_TRANSFER
+        # self.message += STOP_TRANSFER
         self.num_packets = len(self.message)
-        self._buffer = []
-        # self._buffer = queue.Queue(self.window_size)
-
-    def _send_window(self):
-        self.seq_num = 0
-
-        # while self.base_num + self.seq_num < min(self.base_num + self.window_size, len(message)):
-        while self.seq_num < min(self.window_size, self.num_packets - self.base_num):
-            self.send()
-            self.seq_num += 1
-
-        # m_pos = self.base_num
-        # for m_pos in range(self.base_num, min(self.base_num + self.window_size, len(message))):
-        #     self.sender_to_reciever_ch.send(Packet(seq_num=self.seq_num, payload=message[m_pos]))
-        # self.seq_num = m_pos - self.base_num
-
-        # curr_pos_in_message = self.base_num + self.seq_num
-        # while curr_pos_in_message < len(message):
-        #     self.sender_to_reciever_ch.send(Packet(seq_num=self.seq_num, payload=message[curr_pos_in_message]))
-        #     self.seq_num += 1
-        #     curr_pos_in_message += 1
 
     def run(self):
-        while self.base_num < self.num_packets:
-            if reciever_to_sender_ch:
+        left_bound = 0  # left boundary of the window
+        m_pos = 0  # message position
+        last_send_time = 0
+        while left_bound < self.num_packets:
+            if self.reciever_to_sender_ch:
                 packet = self.reciever_to_sender_ch.recieve()
-                self.base_num += 1
-                self.window_pos -= 1
-
-            if self.window_pos < self.window_size:
-                m_pos = self.base_num + self.window_pos
-                self.sender_to_reciever_ch.send(
-                    Packet(
-                        seq_num=m_pos % self.num_packets,
-                        payload=self.message[m_pos],
+                slog.debug(f"Recieved ACK for {packet.seq_num}")
+                if packet.seq_num != (
+                    expected_seq_num := left_bound % self.window_size
+                ):
+                    warnings.warn(
+                        (
+                            f"Recieved ACK {packet.seq_num} doesn't match "
+                            f"the expected seq_num {expected_seq_num}"
+                        ),
+                        UserWarning,
                     )
+                    m_pos = left_bound
+                else:
+                    left_bound += 1
+
+            if m_pos < min(left_bound + self.window_size, self.num_packets):
+                packet = Packet(
+                    seq_num=m_pos % self.window_size,
+                    payload=self.message[m_pos],
                 )
+                slog.debug(f"Sent {packet}")
+                self.sender_to_reciever_ch.send(packet)
+                m_pos += 1
+                self.n_sent += 1
+                last_send_time = time.monotonic()
 
-        # num_packets = len(message)
-        # self._buffer = message[:self.window_size]
-        # for chunk in message[:self.window_size]:
-        #     self._buffer.append(chunk)
+            if time.monotonic() - last_send_time > self.timeout:
+                slog.debug(
+                    f"Timed out, resending from {left_bound} ({self.message[left_bound]})"
+                )
+                m_pos = left_bound
 
+        self.handle_eot()
+
+    def handle_eot(self):
+        eot_packet = Packet(seq_num=EOT, payload="S")
+        # send eot_packets until EOT ACK is recieved
         while True:
-            if reciever_to_sender_ch:
-                self.recieve_and_send()
+            slog.debug("Sending EOT")
+            self.sender_to_reciever_ch.send(eot_packet)
 
-            if self.window_pos == self.window_size - 1:
-                self.wait()
-            else:
-                self.send()
-
-            # transmission complete
-            if self.base_num >= self.num_packets:
-                break
-
-            time.sleep(self.sleep_time)
-
-    def send(self):
-        m_pos = self.base_num + self.window_pos
-        if m_pos < self.num_packets:
-            pkt = Packet(seq_num=m_pos % self.window_size, payload=self.message[m_pos])
-        else:
-            pkt = Packet(seq_num=STOP_TRANSFER, payload="")
-        print(f"Sent {pkt}")
-        self.sender_to_reciever_ch.send(pkt)
-
-    def recieve_and_send(self):
-        pkt = reciever_to_sender_ch.recieve()
-        print(f"Recieved ACK for {pkt}")
-        self.base_num += 1
-        # self.window_pos += 1
-        # self.seq_num = (self.seq_num + 1) % self.window_size
-        self.send()
-
-    def wait(self):
-        start_time = time.time()
-
-        while not reciever_to_sender_ch:
-            time.sleep(self.sleep_time)
-            if time.time() - start_time > self.timeout_time:
-                print("Timed out waiting for reciever's ACK. Resending window")
-                self._send_window()
+            if self.reciever_to_sender_ch:
+                slog.debug("Got EOT ACK")
+                packet = self.reciever_to_sender_ch.recieve()
+                if packet.seq_num == EOT:
+                    break
+            time.sleep(self.timeout)
 
 
 @attrs.define
 class Reciever:
+    """Go-Back-N reciever
+
+    Recieve packets and send ACKs for recieved ones
+    """
+
     sender_to_reciever_ch: PacketQueue
     reciever_to_sender_ch: PacketQueue
     window_size: int = 10
-    expected_seq_num: int = 0
+    n_recieved: int = 0
+    eot_disconnect_timeout: float = 1.0
 
     def run(self):
+        expected_seq_num = 0
+        message = ""
+
+        n_right = 0
+        n_wrong = 0
+
         while True:
             # will block until there is something to recieve
-            packet = sender_to_reciever_ch.recieve()
-            if packet.seq_num == self.expected_seq_num:
-                print(f"Recieved {packet}")
-                reciever_to_sender_ch.send(
-                    Packet(seq_num=self.expected_seq_num, payload="")
-                )
-                print(f"Sent ACK for {self.expected_seq_num} batch packet")
-                self.expected_seq_num = (self.expected_seq_num + 1) % self.window_size
-            else:
-                print(f"Ignoring {packet}: was expecting {self.expected_seq_num}")
+            packet = self.sender_to_reciever_ch.recieve()
+            self.n_recieved += 1
 
             # transfer ended
-            if packet.payload == STOP_TRANSFER:
+            if packet.seq_num == EOT:
+                break
+
+            if packet.seq_num == expected_seq_num:
+                n_right += 1
+                rlog.debug(f"Recieved {packet}")
+                message += packet.payload
+                rlog.debug(f"Sent ACK for {expected_seq_num}")
+                self.reciever_to_sender_ch.send(
+                    Packet(seq_num=expected_seq_num, payload="")
+                )
+                expected_seq_num = (expected_seq_num + 1) % self.window_size
+            else:
+                n_wrong += 1
+                rlog.debug(f"Ignoring {packet}: was expecting {expected_seq_num}")
+                rlog.debug(
+                    f"Sending prev ACK {(expected_seq_num - 1) % self.window_size}"
+                )
+                self.reciever_to_sender_ch.send(
+                    Packet(
+                        seq_num=(expected_seq_num - 1) % self.window_size,
+                        payload="",
+                    )
+                )
+
+        self.handle_eot()
+
+        rlog.debug(f"Recieved message: {message}")
+        rlog.debug(f"{n_right = }")
+        rlog.debug(f"{n_wrong = }")
+
+    def handle_eot(self):
+        eot_packet = Packet(seq_num=EOT, payload="R")
+        resend = True
+        last_send_time = 0
+        while True:
+            if resend:
+                rlog.debug("Sending EOT ACK")
+                self.reciever_to_sender_ch.send(eot_packet)
+                resend = False
+                last_send_time = time.monotonic()
+
+            # we recieve? we send back!
+            # this means, the sender didn't get our EOT ACK
+            if self.sender_to_reciever_ch:
+                packet = self.sender_to_reciever_ch.recieve()
+                rlog.debug("Got EOT again")
+                if packet.seq_num == EOT:
+                    resend = True
+                else:
+                    rlog.debug(f"Expected EOT, got {packet.seq_num}")
+
+            # probably, the sender got the ACK for the EOT packet
+            if time.monotonic() - last_send_time > self.eot_disconnect_timeout:
+                rlog.debug("EOT timeout")
                 break
 
 
 if __name__ == "__main__":
-    loss_probability = 0.3
+    latency = 0.05
+    loss_probability = 0.2
+    window_size = 3
     # a channel from sender to reciever: sender puts, reciever gets
-    sender_to_reciever_ch = PacketQueue(loss_probability=loss_probability)
+    sender_to_reciever_ch = PacketQueue(
+        loss_probability=loss_probability,
+        latency=latency,
+    )
     # a channel from reciever to sender: reciever puts, sender gets
-    reciever_to_sender_ch = PacketQueue(loss_probability=loss_probability)
+    reciever_to_sender_ch = PacketQueue(
+        loss_probability=0.1,
+        latency=latency,
+    )
+
+    from string import ascii_lowercase
+
+    msg = ascii_lowercase[:5]
+    sender = Sender(
+        sender_to_reciever_ch,
+        reciever_to_sender_ch,
+        message=msg,
+        window_size=window_size,
+    )
+    reciever = Reciever(
+        sender_to_reciever_ch,
+        reciever_to_sender_ch,
+        window_size=window_size,
+    )
+
+    # threads = [Thread(target=sender.run), Thread(target=reciever.run)]
+
+    # for th in threads:
+    #     th.start()
+    # for th in threads:
+    #     th.join()
+
+    th_s = Thread(target=sender.run)
+    th_r = Thread(target=reciever.run)
+
+    th_s.start()
+    th_r.start()
+
+    th_s.join()
+    th_r.join()
+
+    print(f"{sender.n_sent = }")
+    print(f"{reciever.n_recieved = }")
