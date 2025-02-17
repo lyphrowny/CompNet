d50e2e8d8d84635f58c6218b8832da1c5224d009
lyphrowny <strukov.as@edu.spbstu.ru>
2025-02-17 12:06:29 +0300

    Clean up non-wiki selective repeat

--- lab1/selective_repeat_non_wiki.py
+++ lab1/selective_repeat_non_wiki.py
@@ -44,10 +44,6 @@ class Sender:
                 )
                 m_pos += 1
 
-            # # shrink buffer so that it's either `window_size` or what's left to get ACK from
-            # while len(_buffer) > min(self.window_size, num_packets - left_bound):
-            #     _buffer.popleft()
-
         # init buffer
         fill_buffer()
         self.log.debug(f"Filled buffer {_buffer}")
@@ -88,25 +84,6 @@ class Sender:
                     packet.sent_at = time.monotonic()
                     self.log.debug(f"Sent packet {packet}")
                     self.s_to_r_stream.send(packet)
-
-            # if self.r_to_s_stream:
-            #     packet = self.r_to_s_stream.recieve()
-            #     self.log.debug(f"Recieved ACK Request {packet.seq_num}")
-            #     diff = (packet.seq_num - left_bound) % seq_mod
-            #     self.log.debug(f"left_bound before {left_bound}, {diff = }")
-            #     left_bound += diff
-            #     self.log.debug(f"left_bound after {left_bound}")
-            #     # add new packets, remove ACKed packets
-            #     fill_buffer(diff)
-            #     self.log.debug(f"Refilled buffer {_buffer}")
-
-            # for packet in _buffer:
-            #     # retransmit lost packets or transmit new packets for the 1st time
-            #     if packet.sent_at + self.timeout < time.monotonic():
-            #         self.n_sent += 1
-            #         packet.sent_at = time.monotonic()
-            #         self.log.debug(f"Sent packet {packet}")
-            #         self.s_to_r_stream.send(packet)
 
 
 @attrs.define
@@ -158,31 +135,5 @@ class Reciever:
                         payload="REQUEST",
                     )
                 )
-
-            # if packet.seq_num == expected_seq_num:
-            #     message += packet.payload
-            #     expected_seq_num = (expected_seq_num + 1) % seq_mod
-
-            #     self.log.debug(f"Buffer before removal {_buffer}")
-            #     while _buffer[expected_seq_num % b_mod] is not None:
-            #         message += _buffer[expected_seq_num % b_mod].payload
-            #         _buffer[expected_seq_num % b_mod] = None
-            #         expected_seq_num = (expected_seq_num + 1) % seq_mod
-            #     self.log.debug(f"Buffer after removal {_buffer}")
-            # # if packet num is within the window
-            # elif (packet.seq_num - expected_seq_num) % seq_mod < b_mod:
-            #     self.log.debug(f"Inserting new packet, before {_buffer}")
-            #     _buffer[packet.seq_num % b_mod] = packet
-            #     self.log.debug(f"Inserted new packet, after {_buffer}")
-            # else:
-            #     self.log.debug("Out of window packet, ignoring")
-
-            # self.log.debug(f"Sent ACK Request {expected_seq_num}")
-            # self.r_to_s_stream.send(
-            #     Packet(
-            #         seq_num=expected_seq_num,
-            #         payload="REQUEST",
-            #     )
-            # )
 
         self.log.debug(f"{message = }")
