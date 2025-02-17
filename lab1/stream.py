8e637f766cfe51c0ab41ac50a1e3e4148e769acf
lyphrowny <strukov.as@edu.spbstu.ru>
2025-02-17 09:50:08 +0300

    Change prefix for Stream logger P->p

--- lab1/stream.py
+++ lab1/stream.py
@@ -16,7 +16,11 @@ EOT = -1
 class Packet:
     seq_num: int
     payload: str
-    sent_at: int = attrs.field(default=0)
+    sent_at: int = attrs.field(
+        default=0,
+        # 123.456 sec but as int (123_456)
+        # repr=lambda t: f"{int(t // 1e6 % 1e6):_}",
+    )
 
 
 @attrs.define
@@ -24,7 +28,7 @@ class PacketQueue:
     loss_probability: float = 0.3
     latency: float = 0.1  # in sec
     _queue: queue.Queue[Packet] = attrs.field(init=False, factory=queue.Queue)
-    log: logging.Logger = attrs.field(init=False, default=get_logger("P"))
+    log: logging.Logger = attrs.field(init=False, default=get_logger("p"))
 
     def send(self, packet: Packet):
         # emulate network latency
