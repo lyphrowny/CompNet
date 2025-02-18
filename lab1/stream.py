649d477c8e7532507415a30af5697b241a0d7261
lyphrowny <strukov.as@edu.spbstu.ru>
2025-02-18 11:25:08 +0300

    Replace instance logger with global one; Add seeded random in PacketQueue

--- lab1/stream.py
+++ lab1/stream.py
@@ -14,22 +14,31 @@ EOT = -1
 @attrs.define
 class Packet:
     seq_num: int
-    payload: str
+    payload: str = attrs.field(repr=lambda v: f"{v!r}")
     sent_at: float = attrs.field(default=0.0)
+
+
+plog = get_logger("p")
 
 
 @attrs.define
 class PacketQueue:
     loss_probability: float = 0.3
     latency: float = 0.1  # in sec
-    _queue: queue.Queue[Packet] = attrs.field(init=False, factory=queue.Queue)
-    log: logging.Logger = attrs.field(init=False, default=get_logger("p"))
+    _queue: queue.Queue[Packet] = attrs.field(
+        init=False,
+        factory=queue.Queue,
+        alias="_queue",
+    )
+    _rng: random.Random = attrs.field(init=False, alias="_rng")
+
+    def __attrs_post_init__(self):
+        self._rng = random.Random(42)
 
     def send(self, packet: Packet):
-        # emulate network latency
         time.sleep(self.latency)
-        if random.random() < self.loss_probability:
-            self.log.debug(f"{packet} was lost!")
+        if self._rng.random() < self.loss_probability:
+            plog.debug(f"{packet} was lost!")
         else:
             self._queue.put(packet)
 
